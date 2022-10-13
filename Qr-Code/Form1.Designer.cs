8a1bd2fd0c61cd661e3cff6f895bf07b651f53bd
Heis97 <heis97@mail.ru>
2022-10-13 22:31:29 +0300

    Добавьте файлы проекта.

---
+++ Qr-Code/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace Qr_Code
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(384, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(541, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(384, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name DXF";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "distanse between points";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(142, 85);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(384, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(704, 116);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(30, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(619, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "pixels in square";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 427);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Qr-Code/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+
+namespace Qr_Code
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "https://www.google.com/";
+            textBox2.Text = "Qrcod1";
+            textBox3.Text = "1.0";
+
+            textBox4.Text = "3";
+
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCodeWriter qrEncode = new QRCodeWriter(); //создание QR кода
+
+            string str = textBox1.Text;  //строка на русском языке
+            double k = Convert.ToDouble(textBox3.Text)/2;
+            string name_dxf = textBox2.Text;
+            int pix = Convert.ToInt32(textBox4.Text);
+            Dictionary<EncodeHintType, object> hints = new Dictionary<EncodeHintType, object>();    //для колекции поведений
+            hints.Add(EncodeHintType.CHARACTER_SET, "utf-8");   //добавление в коллекцию кодировки utf-8
+            BitMatrix qrMatrix = qrEncode.encode(   //создание матрицы QR
+                str,                 //кодируемая строка
+                BarcodeFormat.QR_CODE,  //формат кода, т.к. используется QRCodeWriter применяется QR_CODE
+                25,                    //ширина
+                25,                    //высота
+                hints);                 //применение колекции поведений
+            BarcodeWriter qrWrite = new BarcodeWriter();    //класс для кодирования QR в растровом файле
+            Bitmap qrImage = qrWrite.Write(qrMatrix);   //создание изображения
+            try
+            {
+                qrImage.Save("1.bmp", System.Drawing.Imaging.ImageFormat.Bmp);//сохранение изображения
+            }
+            catch
+            {
+
+            }
+            
+            pictureBox1.Image = (Image)(new Bitmap((Image)qrImage, pictureBox1.Width, pictureBox1.Height));
+            BarcodeReader qrDecode = new BarcodeReader(); //чтение QR кода
+            Result text = qrDecode.Decode((Bitmap)Bitmap.FromFile("1.bmp")); //декодирование растрового изображения
+
+            var dxf = new DxfMaker(name_dxf);
+            for (int x = 0; x < qrImage.Width; x += pix)
+            {
+                for (int y = 0; y < qrImage.Height; y += pix)
+                {
+                    if (qrImage.GetPixel(x, y).R < 100)
+                    {
+                        dxf.addPoint(x, y);
+                    }
+                }
+            }
+            dxf.saveDxf(name_dxf+".dxf");
+        }
+    }
+    public class DxfMaker
+    {
+        string name;
+        string begintext;
+        string middletext;
+        string endtext;
+        string text;
+        string textAll;
+        int count;
+        public DxfMaker(string iName)
+        {
+            name = iName;
+            textAll = "";
+            count = 75;
+        }
+
+        public void addPoint(float x, float y)
+        {
+            text += "POINT\n5\n" + count.ToString() + "\n330\n1F\n100\nAcDbEntity\n8\n0\n6\nCONTINUOUS\n62\n7\n100\nAcDbPoint\n10\n" + x.ToString() + "\n20\n" + y.ToString() + "\n30\n0.0\n0\n";
+            count++;
+        }
+        public void saveDxf(string fpath)
+        {
+            extractTexts();
+            textAll = begintext+"\n"+ count.ToString() + "\n" + middletext + "\n" + text + endtext;
+            try
+            {
+                System.IO.StreamWriter fio = new System.IO.StreamWriter(fpath);
+                fio.Write(textAll);
+                fio.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        private string readFile(string path)
+        {
+            string file1="";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                file1 = sr.ReadToEnd();
+            }
+            return file1;
+        }
+        private void extractTexts()
+        {
+            begintext = readFile("Texts\\beginText.txt");
+            middletext = readFile("Texts\\middleText.txt");
+            endtext = readFile("Texts\\endText.txt");
+        }
+
+
+    }
+    
+}
